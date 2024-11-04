c50ff2a1b9d132f17cf20db92416f7c93cc0fe47
Quan Nguyen <jacob.nguyenaq@gmail.com>
2024-11-03 20:01:00 -0700

    Edited readme and added solution for problem TwoSum.

---
+++ problem1.py
@@ -0,0 +1,26 @@
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+:type nums: List[int]
+:type target: int
+:rtype: List[int]
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+"""
+
+# This one consists of two loops where it checks all the possible options.
+# The time complexity for this is O(n^2)
+def firstApproach(self,  nums, target):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return 0
+
