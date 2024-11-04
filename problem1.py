176792049ef715d10451a26e5d185f52f6ecc027
Quan Nguyen <jacob.nguyenaq@gmail.com>
2024-11-03 20:14:40 -0700

    Added optimal solution to problem 1.

--- problem1.py
+++ problem1.py
@@ -17,10 +17,26 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 # This one consists of two loops where it checks all the possible options.
 # The time complexity for this is O(n^2)
-def firstApproach(self,  nums, target):
+def firstApproach(nums, target):
     for i in range(len(nums)):
         for j in range(i+1, len(nums)):
             if nums[i] + nums[j] == target:
                 return [i, j]
     return 0
 
+# This one has only one loop where it uses the dictionary to store the "wrong answers" which uses subtraction to determine. If the answer is in the dictionary, then that's the answer.
+# The time complexity for this is O(n).
+def secondApproach(nums, target):
+    dict = {}
+    for i in range(len(nums)):
+        if target - nums[i] in dict:
+            return [i, dict[target-nums[i]]]
+        else:
+            dict[nums[i]]= i
+    return 0
+
+def main():
+    print(firstApproach([3,2,4], 6))
+    print(secondApproach([3,2,4], 6))
+
+main()
