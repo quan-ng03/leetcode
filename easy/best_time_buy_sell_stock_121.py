b53ec99466258b8e2353a6072b9cee53de869748
Quan Nguyen <jacob.nguyenaq@gmail.com>
2025-01-25 17:34:23 -0700

    Solved 121

--- easy/best_time_buy_sell_stock_121.py
+++ easy/best_time_buy_sell_stock_121.py
@@ -1,10 +1,27 @@
 
 """
-
+    So return the largest profit by buying and selling within the days range.
+    1. Find the lowest price and set it as the min
+    2. Find the highest profit starting at the min price
 """
 def maxProfit(prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
+    """
+    :type prices: List[int]
+    :rtype: int
+    """
+    if len(prices) == 1:
+        return 0
+    
+    min  = prices[0]
+    profit = 0
+    
+    for i in range(1, len(prices)):
+        if min > prices[i]:
+            min = prices[i]
+        else:
+            x = prices[i]-min
+            if x > profit:
+                profit = x
+    return profit
+    
         
---
+++ tests/test_best_time_buy_sell_stock_121.py
@@ -0,0 +1,9 @@
+import unittest
+from easy.best_time_buy_sell_stock_121 import maxProfit
+
+class TestMaxProfit(unittest.TestCase):
+    def test(self):
+        self.assertEqual(maxProfit([7,1,5,3,6,4]), 5)
+        self.assertEqual(maxProfit([7,6,4,3,1]), 0)
+if __name__ == "__main__":
+    unittest.main()
