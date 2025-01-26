d28833300373c6ca2b6f641d3da0318b28b23284
Quan Nguyen <jacob.nguyenaq@gmail.com>
2025-01-25 18:34:26 -0700

    Add 704 binary search

---
+++ easy/binary_search_704.py
@@ -0,0 +1,27 @@
+
+"""
+
+"""
+
+
+def search(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    l = 0
+    r = len(nums)-1
+    while l <= r:
+        x = (l + r) // 2
+        if nums[x] == target:
+            return x
+        elif nums[x] > target:
+            r = x - 1
+        elif nums[x] < target:
+            l = x + 1
+    return -1
+
+        
+
+        
---
+++ tests/test_binary_search_704.py
@@ -0,0 +1,9 @@
+import unittest
+from easy.binary_search_704 import search
+
+class TestMaxAchieveNum(unittest.TestCase):
+    def test(self):
+        self.assertEqual(search([-1,0,3,5,9,12],9), 4)
+        self.assertEqual(search([-1,0,3,5,9,12],2), -1)
+if __name__ == "__main__":
+    unittest.main()
